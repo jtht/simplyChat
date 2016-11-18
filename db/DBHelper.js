6d85e9ec91a23ab80dfe379231b25b0331aa6fe4
Jóhannes Þorkell Tómasson <jtht2@hi.is>
2016-11-18 03:43:04 +0000

    fyrsta útgáfa sem virkar á heroku

--- db/DBHelper.js
+++ db/DBHelper.js
@@ -21,16 +21,6 @@ var config = {
   idleTimeoutMillis: 1000 //close idle clients after 1 second
 };
 
-// local stillingar
-// var config = {
-//   user: 'johannesthorkell', //env var: PGUSER
-//   database: 'puppies',
-//   host: 'localhost', // Server hosting the postgres database
-//   port: 5432, //env var: PGPORT
-//   max: 10, // max number of clients in the pool
-//   idleTimeoutMillis: 1000, // how long a client is allowed to remain idle before being closed
-// };
-
 var pool = new pg.Pool(config);
 
 function DBHelper() {}
